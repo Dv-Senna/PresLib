d9d0bee5a343e59d7ac4b690637505b0f66065a0
dvsenna64 <dvsenna64@gmail.com>
2023-06-22 20:14:11 +0200

    A whole rewriting is needed to add a compile time multi-rendering support. This commit is there to clean the repo

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 
 #include "pl/preslib.hpp"
-#include "pl/themes/modern.hpp"
 
 
 
@@ -11,65 +10,7 @@ int main(int argc, char *argv[])
 {
 	try
 	{
-		pl::Instance instance {};
-
-		pl::themes::Modern theme {instance};
-		theme.use();
-
-
-		instance.getFonts().addFont("arial", "arial.ttf");
-
-		std::cout << instance.getTitlePosition() << std::endl;
-
-
-
-		pl::Slide slide {instance, pl::SlideFlag::no_background | pl::SlideFlag::no_overlay};
-		instance.addSlide(&slide);
-
-		pl::block::Line line {instance, {10, 20}, {30, 40}};
-		slide.addChildren(&line);
-
-		pl::block::Ellipse circle {instance, {100, 100}, 30, 0.0f, pl::utils::colorUndefined, pl::DrawingType::outlined};
-		slide.addChildren(&circle);
-
-
-		pl::Slide slide2 {instance};
-		instance.addSlide(&slide2);
-
-		pl::block::Ellipse ellipse {instance, {400, 400}, 20, 0.9f};
-		ellipse.flipWidthAndHeight();
-		slide2.addChildren(&ellipse);
-
-		pl::block::Text text {instance, {500, 500}, "Hello World from PresLib !", "arial", 20};
-		slide2.addChildren(&text);
-
-		pl::block::Text text1 {instance, {500, 600}, "Hello Body from PresLib !", "arial", 50};
-		slide2.addChildren(&text1);
-
-		pl::block::Image image {instance, "image.png", {384, 583}, 2.0f};
-		slide2.addChildren(&image);
-
-		pl::block::Triangle triangle {
-			instance,
-			{pl::math::Vec2(100.0f, 100.0f), pl::math::Vec2(150.0f, 150.0f), pl::math::Vec2(150.0f, 100.0f)},
-			pl::utils::colorUndefined,
-			pl::DrawingType::outlined
-		};
-		slide2.addChildren(&triangle);
-
-		pl::block::Math equation {instance,
-			"\\mathscr{L} = \\dfrac{1}{2}\\partial_\\mu\\phi\\partial^\\mu\\phi - \\dfrac{1}{2}m^2\\phi^2",
-			{50, 50}, {255, 150, 0}};
-		slide2.addChildren(&equation);
-
-
-
-		pl::Slide themeSlide {instance};
-		instance.addSlide(&themeSlide);
-		themeSlide.setTitle("Un titre incroyable | modern");
-	
-
-		instance.run();
+		
 	}
 
 	catch (const std::exception &exception)
