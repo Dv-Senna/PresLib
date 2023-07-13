b4fedfcf1a9b4264022072be80475e4cd33d74fb
dvsenna64 <dvsenna64@gmail.com>
2023-07-13 22:09:31 +0200

    Add antialiazing to ellipse

--- lib/include/pl/impl/SDL2_renderer/blocks/ellipse.hpp
+++ lib/include/pl/impl/SDL2_renderer/blocks/ellipse.hpp
@@ -25,10 +25,22 @@ namespace pl::impl::SDL2_renderer::blocks
 			virtual void render();
 
 		private:
+			static void s_drawFilledEllipse(
+				SDL_Surface *surface,
+				const pl::math::Vec2f &size
+			);
+
+			static void s_drawBorderEllipse(
+				SDL_Surface *surface,
+				const pl::math::Vec2f &outerSize,
+				const pl::math::Vec2f &innerSize
+			);
+
 			pl::math::Vec2f m_center, m_size;
 			float m_angle;
 			pl::utils::Color m_color;
 			pl::RenderMethod m_method;
+			SDL_Texture *m_texture;
 	};
 
 } // namespace pl::impl::SDL2_renderer::blocks
--- lib/src/impl/SDL2_renderer/blocks/ellipse.cpp
+++ lib/src/impl/SDL2_renderer/blocks/ellipse.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <memory>
 #include <stdexcept>
 
 #include <SDL2/SDL2_gfxPrimitives.h>
@@ -20,12 +22,40 @@ namespace pl::impl::SDL2_renderer::blocks
 	) : 
 		pl::impl::Block(instance),
 		m_center {center},
-		m_size {radius, radius * sqrt(1 - excentricity)},
+		m_size {radius, radius * sqrt(1 - excentricity * excentricity)},
 		m_angle {angle},
 		m_color {color},
-		m_method {method}
+		m_method {method},
+		m_texture {nullptr}
 	{
+		constexpr float borderSize = 4.f;
+		const pl::math::Vec2f innerBorder {m_size.x - borderSize, m_size.y - borderSize};
 
+		std::unique_ptr<SDL_Surface, void (*)(SDL_Surface*)> surface {SDL_CreateRGBSurfaceWithFormat(
+				0, m_size.x * 2, m_size.y * 2, 8, SDL_PIXELFORMAT_RGBA8888
+			), SDL_FreeSurface};
+
+		if (surface.get() == nullptr)
+			throw std::runtime_error("PL : Can't create the surface of ellispe : " + std::string(SDL_GetError()));
+
+
+		if (m_method == pl::RenderMethod::fill)
+			s_drawFilledEllipse(surface.get(), m_size);
+
+		else if (m_method == pl::RenderMethod::border)
+			s_drawBorderEllipse(surface.get(), m_size, innerBorder);
+
+
+		m_texture = SDL_CreateTextureFromSurface(
+			std::any_cast<SDL_Renderer*> (m_instance->getHandler()),
+			surface.get()
+		);
+
+		if (m_texture == nullptr)
+			throw std::runtime_error("PL : Can't convert the surface of ellispe in texture : " + std::string(SDL_GetError()));
+
+		if (SDL_SetTextureColorMod(m_texture, m_color.r, m_color.g, m_color.b) != 0)
+			throw std::runtime_error("PL : Can't set ellipse's texture color mod : " + std::string(SDL_GetError()));
 	}
 
 
@@ -39,53 +69,127 @@ namespace pl::impl::SDL2_renderer::blocks
 
 	void Ellipse::render()
 	{
-		/*if (SDL_SetRenderDrawColor(std::any_cast<SDL_Renderer*> (m_instance->getHandler()),
-				m_color.r, m_color.g, m_color.b, m_color.a
-			) != 0)
-			throw std::runtime_error("PL : Can't change ellipse draw color : " + std::string(SDL_GetError()));*/
-
-
-		if (m_size.x == m_size.y)
-		{
-			if (m_method == pl::RenderMethod::border)
-			{
-				if (aacircleRGBA(std::any_cast<SDL_Renderer*> (m_instance->getHandler()),
-					m_center.x, m_center.y, m_size.x, m_color.r, m_color.g, m_color.b, m_color.a
-				) != 0)
-					throw std::runtime_error("PL : Can't render outlined circle : " + std::string(SDL_GetError()));
-
-				return;
-			}
-
-			if (filledCircleRGBA(std::any_cast<SDL_Renderer*> (m_instance->getHandler()),
-				m_center.x, m_center.y, m_size.x, m_color.r, m_color.g, m_color.b, m_color.a
-			) != 0)
-				throw std::runtime_error("PL : Can't render filled circle : " + std::string(SDL_GetError()));
-
+		if (m_texture == nullptr)
 			return;
-		}
 
+		SDL_FRect rect {m_center.x - m_size.x, m_center.y - m_size.y, m_size.x * 2.0f, m_size.y * 2.0f};
 
-
-		if (m_method == pl::RenderMethod::border)
-		{
-			if (aaellipseRGBA(
-				std::any_cast<SDL_Renderer*> (m_instance->getHandler()),
-				m_center.x, m_center.y, m_size.x, m_size.y, m_color.r, m_color.g, m_color.b, m_color.a
+		if (SDL_RenderCopyExF(
+			std::any_cast<SDL_Renderer*> (m_instance->getHandler()),
+			m_texture, nullptr, &rect, m_angle, nullptr, SDL_FLIP_NONE
 			) != 0)
-				throw std::runtime_error("PL : Can't render outlined ellipse : " + std::string(SDL_GetError()));
+			throw std::runtime_error("PL : Can't render ellipse texture : " + std::string(SDL_GetError()));
 
-			return;
-		}
-		
-		if (filledEllipseRGBA(
-				std::any_cast<SDL_Renderer*> (m_instance->getHandler()),
-				m_center.x, m_center.y, m_size.x, m_size.y, m_color.r, m_color.g, m_color.b, m_color.a
-			) != 0)
-				throw std::runtime_error("PL : Can't render filled ellipse : " + std::string(SDL_GetError()));
 
 
 		this->m_renderChildren();
+	}
+
+
+
+	void Ellipse::s_drawFilledEllipse(
+		SDL_Surface *surface,
+		const pl::math::Vec2f &size
+	)
+	{
+		constexpr float antialiasingPourcentage {0.97f};
+		float a = - M_PI / (2.f * (1 - antialiasingPourcentage * antialiasingPourcentage));
+		float b = -a;
+
+		for (int x {0}; x < static_cast<int> (size.x * 2.0f); x++)
+		{
+			for (int y {0}; y < static_cast<int> (size.y * 2.0f); y++)
+			{
+				uint32_t *currentPixel = reinterpret_cast <uint32_t*> (static_cast<uint8_t*> (
+					surface->pixels
+				) + y * surface->pitch + x * surface->format->BytesPerPixel);
+
+				*currentPixel = pl::utils::transparent;
+				
+				float xoffset = static_cast<float> (x) - size.x;
+				float yoffset = static_cast<float> (y) - size.y;
+				float length = xoffset * xoffset / size.x / size.x + yoffset * yoffset / size.y / size.y;
+
+				if (length <= antialiasingPourcentage * antialiasingPourcentage)
+					*currentPixel = pl::utils::white;
+
+				else if (length <= 1.0f)
+				{
+					int alpha = 255 * sin(a * length + b);
+					*currentPixel = pl::utils::Color(255, 255, 255, alpha);
+				}
+			}
+		}
+	}
+
+
+
+	void Ellipse::s_drawBorderEllipse(
+		SDL_Surface *surface,
+		const pl::math::Vec2f &outerSize,
+		const pl::math::Vec2f &innerSize
+	)
+	{
+		constexpr float maxAntialiasingPourcentage {0.97f};
+		float outerAntialiasingPourcentage = maxAntialiasingPourcentage;
+		float innerAntialiasingPourcentage = 2.f - maxAntialiasingPourcentage;
+
+		/*
+		// this section is not working as indented
+
+		if (outerSize.x * outerSize.y * outerAntialiasingPourcentage
+			< innerSize.x * innerSize.y * innerAntialiasingPourcentage
+		)
+		{
+			outerAntialiasingPourcentage = (outerSize.x * outerSize.y + innerSize.x * innerSize.y)
+				/ (2.f * outerSize.x * outerSize.y);
+			innerAntialiasingPourcentage = (outerSize.x * outerSize.y + innerSize.x * innerSize.y)
+				/ (2.f * innerSize.x * innerSize.y);
+		}*/
+
+		float outerA = - M_PI / (2.f * (1 - outerAntialiasingPourcentage * outerAntialiasingPourcentage));
+		float outerB = -outerA;
+		float innerA = - M_PI / (2.f * (1 - innerAntialiasingPourcentage * innerAntialiasingPourcentage));
+		float innerB = -innerA;
+
+
+		for (int x {0}; x < static_cast<int> (outerSize.x * 2.0f); x++)
+		{
+			for (int y {0}; y < static_cast<int> (outerSize.y * 2.0f); y++)
+			{
+				uint32_t *currentPixel = reinterpret_cast <uint32_t*> (static_cast<uint8_t*> (
+					surface->pixels
+				) + y * surface->pitch + x * surface->format->BytesPerPixel);
+
+				*currentPixel = pl::utils::transparent;
+
+				float xoffset = static_cast<float> (x) - outerSize.x;
+				float yoffset = static_cast<float> (y) - outerSize.y;
+				float outerLength = xoffset * xoffset / outerSize.x / outerSize.x + yoffset * yoffset / outerSize.y / outerSize.y;
+				float innerLength = xoffset * xoffset / innerSize.x / innerSize.x + yoffset * yoffset / innerSize.y / innerSize.y;
+
+				if (outerLength <= outerAntialiasingPourcentage * outerAntialiasingPourcentage
+					&& innerLength >= innerAntialiasingPourcentage * innerAntialiasingPourcentage
+				)
+					*currentPixel = pl::utils::white;
+
+				else if (outerLength <= 1.f
+					&& innerLength >= innerAntialiasingPourcentage * innerAntialiasingPourcentage
+				)
+				{
+					int alpha = 255 * sin(outerA * outerLength + outerB);
+					*currentPixel = pl::utils::Color(255, 255, 255, alpha);
+				}
+
+				else if (outerLength <= outerAntialiasingPourcentage * outerAntialiasingPourcentage
+					&& innerLength >= 1.f
+				)
+				{
+					int alpha = 255 * sin(innerA * innerLength + innerB);
+					*currentPixel = pl::utils::Color(255, 255, 255, alpha);
+				}
+			}
+		}
 	}
 
 
